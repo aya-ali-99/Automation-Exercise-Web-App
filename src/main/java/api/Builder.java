94d41b20a520cebde7574c814174f3354f6e0f8b
aya-ali-99 <ayaali969@gmail.com>
2025-11-01 19:28:03 +0200

    Added API Request Management classes

---
+++ src/main/java/api/Builder.java
@@ -0,0 +1,25 @@
+package api;
+
+import automationexercices.utils.dataReader.PropertyReader;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+
+public class Builder {
+    private static String baseURI = PropertyReader.getProperty("baseUrlApi");
+
+    private Builder(){
+        // Private constructor to prevent instantiation
+    }
+
+    // Build request specification
+    public static RequestSpecification getUserManagementRequestSpecification(Map<String, ?> formParams) {
+        return new RequestSpecBuilder()
+                .setBaseUri(baseURI)
+                .setContentType(ContentType.URLENC)
+                .addFormParams(formParams)
+                .build();
+    }
+}
