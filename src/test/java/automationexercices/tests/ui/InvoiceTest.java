3fcd4293759c7d1e69b4206b9d1cdf18787f45a3
aya-ali-99 <ayaali969@gmail.com>
2025-11-05 00:58:56 +0200

    Added PaymentPage, PaymentTest and InvoiceTest

---
+++ src/test/java/automationexercices/tests/ui/InvoiceTest.java
@@ -0,0 +1,159 @@
+package automationexercices.tests.ui;
+
+import api.UserManagementAPI;
+import automationexercices.drivers.GUIDriver;
+import automationexercices.pages.*;
+import automationexercices.pages.components.NavigationBarComponent;
+import automationexercices.tests.BaseTest;
+import automationexercices.utils.TimeManager;
+import automationexercices.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+@Epic("Automation Exercise ")
+@Feature("UI Invoice Management")
+@Story("Invoice Management")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Aya")
+public class InvoiceTest extends BaseTest {
+    String timeStamp = TimeManager.getSimpleTimestamp();
+
+    // Tests
+    @Description("Register new account")
+    @Test
+    public void registerNewAccountTC(){
+        new UserManagementAPI().createRegisterUserAccount(
+                        testData.getJsonData("name"),
+                        (testData.getJsonData("email")+ timeStamp +"@gmail.com"),
+                        testData.getJsonData("password"),
+                        testData.getJsonData("titleFemale"),
+                        testData.getJsonData("day"),
+                        testData.getJsonData("month"),
+                        testData.getJsonData("year"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("mobileNumber"))
+                .verifyUserIsCreatedSuccessfully();
+    }
+
+    @Description("Login to account")
+    @Test(dependsOnMethods = {"registerNewAccountTC"})
+    public void loginToAccountTC(){
+        new SignupLoginPage(driver).navigate()
+                .enterLoginEmail(testData.getJsonData("email")+ timeStamp+"@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password"))
+                .clickLoginButton()
+                .navigationBar
+                .verifyUserLabel(testData.getJsonData("name"));
+    }
+
+    @Description("Add product to cart")
+    @Test(dependsOnMethods = {"loginToAccountTC", "registerNewAccountTC"})
+    public void addProductToCartTC(){
+        new ProductsPage(driver)
+                .navigate()
+                .clickOnAddToCart(testData.getJsonData("product.name"))
+                .validateItemAddedLabel(testData.getJsonData("messages.cartAdded"))
+                .clickOnViewCart()
+                .verifyProductDetailsInCart(
+                        testData.getJsonData("product.name"),
+                        testData.getJsonData("product.price"),
+                        testData.getJsonData("product.quantity"),
+                        testData.getJsonData("product.total"));
+    }
+
+    @Description("Checkout")
+    @Test(dependsOnMethods = {"addProductToCartTC", "loginToAccountTC", "registerNewAccountTC"})
+    public void checkoutTC(){
+        new CartPage(driver)
+                .navigate()
+                .clickOnProceedToCheckoutButton()
+                .verifyDeliveryAddress(
+                        testData.getJsonData("titleFemale"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("mobileNumber")
+                )
+                .verifyBillingAddress(
+                        testData.getJsonData("titleFemale"),
+                        testData.getJsonData("firstName"),
+                        testData.getJsonData("lastName"),
+                        testData.getJsonData("companyName"),
+                        testData.getJsonData("address1"),
+                        testData.getJsonData("address2"),
+                        testData.getJsonData("city"),
+                        testData.getJsonData("state"),
+                        testData.getJsonData("zipcode"),
+                        testData.getJsonData("country"),
+                        testData.getJsonData("mobileNumber")
+                );
+
+    }
+
+    @Description("Payment")
+    @Test(dependsOnMethods = {"checkoutTC", "addProductToCartTC", "loginToAccountTC", "registerNewAccountTC"})
+    public void paymentTC(){
+        new CheckoutPage(driver)
+                .clickPlaceOrderBtn()
+                .fillCardInfo(
+                        testData.getJsonData("card.cardName"),
+                        testData.getJsonData("card.cardNumber"),
+                        testData.getJsonData("card.cvc"),
+                        testData.getJsonData("card.exMonth"),
+                        testData.getJsonData("card.exYear")
+                )
+                .clickPayAndConfirmBtn()
+                .verifySuccessMessage()
+                .clickDownloadInvoiceBtn();
+    }
+
+    @Description("Verify invoice file")
+    @Test(dependsOnMethods = {"paymentTC", "checkoutTC", "addProductToCartTC", "loginToAccountTC", "registerNewAccountTC"})
+    public void verifyInvoiceFileTC(){
+        new PaymentPage(driver)
+                .clickDownloadInvoiceBtn()
+                .verifyDownloadInvoiceFile(
+                        testData.getJsonData("invoiceName")
+                );
+    }
+
+    @Description("Delete account")
+    @Test(dependsOnMethods = {"verifyInvoiceFileTC", "paymentTC", "checkoutTC", "addProductToCartTC", "loginToAccountTC", "registerNewAccountTC"})
+    public void deleteAccountTC(){
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email")+ timeStamp+"@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserIsDeletedSuccessfully();
+    }
+
+
+    // Configurations
+    @BeforeClass
+    public void setUp(){
+        testData = new JsonReader("checkout-data");
+        driver = new GUIDriver();
+        new NavigationBarComponent(driver).navigate();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quitDriver();
+    }
+}
