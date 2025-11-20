c58de9194fce23e0b00b790a5269126444928bc8
aya-ali-99 <ayaali969@gmail.com>
2025-11-20 02:10:05 +0200

    Fixed some locators

--- src/main/java/automationexercices/pages/ProductsPage.java
+++ src/main/java/automationexercices/pages/ProductsPage.java
@@ -29,19 +29,19 @@ public class ProductsPage {
 
     // Dynamic Locators
     private By productName(String productName) {
-        return By.xpath("//div[@class='overlay-content'] /p[.='" + productName + "']");
+        return By.xpath("//div[@class='features_items'] //div[@class='overlay-content'] /p[.='" + productName + "']");
     }
 
     private By productPrice(String productName) {
-        return By.xpath("//div[@class='overlay-content'] /p[.='" + productName + "'] //preceding-sibling::h2");
+        return By.xpath("//div[@class='features_items'] //div[@class='overlay-content'] /p[.='" + productName + "'] //preceding-sibling::h2");
     }
 
     private By hoverOnProduct(String productName) {
-        return By.xpath("//div[@class='productinfo text-center'] /p[.='" + productName + "']");
+        return By.xpath("//div[@class='features_items'] //div[@class='productinfo text-center'] /p[.='" + productName + "']");
     }
 
     private By addToCartButton(String productName) {
-        return By.xpath("//div[@class='productinfo text-center'] /p[.='" + productName + "'] //following-sibling::a");
+        return By.xpath("//div[@class='features_items'] //div[@class='productinfo text-center'] /p[.='" + productName + "'] //following-sibling::a");
     }
 
     private By viewProduct(String productName) {
